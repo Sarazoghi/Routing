855def337e59ac36f03acafb1368f5a4f826bc12
Mahdi Soleimani <mahdisoleimani101@gmail.com>
2023-12-07 19:07:34 +0330

    file to process testcases converting all inputs to numbers

---
+++ testcases.py
@@ -0,0 +1,34 @@
+# rows, cols = [int(item) for item in input().split()]
+# city_map = []
+# for row in range(rows):
+#     city_map.append(input().split())
+rows, cols = 6, 10
+city_map = [
+    ['1R', '1', '1', '5', '5', '4', '2C', '1', '15', '1B'],
+    ['1', '1', '5', '3', '5', '5', '4', '5', 'X', 'X'],
+    ['5', '1I', '1', '6', '2', '2', '2', '1', '1', '1T'],
+    ['X', 'X', '1', '6', '5', '5', '2', '1', '1', 'X'],
+    ['X', 'X', '1', 'X', 'X', '50', '2', '1C', '1', 'X'],
+    ['1', '1', '1', '2', '2', '2T', '2', '1', '1', '1'],
+]
+target_coord = []
+START_ENERGY = 500
+bonus_values = {
+    'C': 10,
+    'B': 5,
+    'I': 12
+}
+
+for row in range(rows):
+    for index in range(cols):
+        if city_map[row][index].isdigit():
+            city_map[row][index] = -int(city_map[row][index])
+        elif city_map[row][index][-1:] == 'R':
+            city_map[row][index] = -int(city_map[row][index][:-1])
+        elif city_map[row][index][-1:] == 'T':
+            target_coord.append((row, index))
+            city_map[row][index] = -int(city_map[row][index][:-1])
+        elif city_map[row][index] == 'X':
+            city_map[row][index] = float('-inf')
+        elif city_map[row][index][-1:] == 'C' or city_map[row][index][-1:] == 'B' or city_map[row][index][-1:] == 'I':
+            city_map[row][index] = -int(city_map[row][index][:-1]) + bonus_values[city_map[row][index][-1:]]
