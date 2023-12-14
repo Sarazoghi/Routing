89aea783c4f9c6b6b1d9a999b2c9e5ac126a698f
mahdisoleimanii <mahdisoleimani101@gmail.com>
2023-12-14 17:54:43 +0330

    astar algorithm completed

--- astar_alg.py
+++ astar_alg.py
@@ -1,14 +1,21 @@
 from testcases import target_coord, city_map
-from succ_func import successor_func, State
+from succ_func import successor_func, State, manhat_heu
 
 start_states = []
 for target in target_coord:
     start_states.extend(successor_func(State(target)))
-start_state = start_states[0]
-for st_state in start_states:
-    if st_state.getTotalVal() < start_state.getTotalVal():
-        start_state = st_state
+start_state = min(start_states)
+
+def astar(state: State, not_visited: list = target_coord.copy()):
+    print(state.getInfo())
+    best_next_move = min(successor_func(state))
+    if state.getCurrent() == state.getTarget():
+        not_visited.remove(state.getCurrent())
+        if not_visited:
+            best_next_target = min(not_visited, key=lambda target: manhat_heu(state.getCurrent(), target))
+            best_next_move.target = best_next_target
+            astar(best_next_move, not_visited)
+    else:
+        astar(best_next_move, not_visited)
         
-def astar(state: State):
-    successor_states = successor_func(state)
-    
+astar(start_state)
