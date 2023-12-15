ba9f52f67e9590590792b1e2433c026404e8627e
mahdisoleimanii <mahdisoleimani101@gmail.com>
2023-12-15 18:11:12 +0330

    greedy algorithm file created

---
+++ greedy_alg.py
@@ -0,0 +1,36 @@
+from testcases import *
+from succ_func import *
+import timeit
+
+def greedy(start_state: State):
+    if start_state.getCurrent() == start_state.getTarget():
+        return start_state
+    else:
+        successor_states = successor_func(start_state)
+        best_next_state = min(successor_states)
+        print(start_state.getInfo(), best_next_state.getInfo())
+        if best_next_state.getHeuVal() > start_state.getHeuVal():
+            return None
+        return greedy(best_next_state)
+
+start_time = timeit.default_timer()
+
+targets = target_coord.copy()
+start_states = []
+for target in target_coord:
+    start_states.extend(successor_func(State(target)))
+
+final_state = min(start_states)
+while targets and final_state:
+    best_next_target = min(targets, key=lambda target: manhat_heu(final_state.getCurrent(), target))
+    final_state.setTarget(best_next_target)
+    final_state = greedy(final_state)
+    targets.remove(best_next_target)
+
+end_time = timeit.default_timer()
+
+if final_state:
+    total_time = f'{end_time - start_time} seconds'
+    print(f"Steps: {convert_to_str(final_state.getSteps())} \nEnergy: {final_state.getEnergy()} \nTime: {total_time}")
+else:
+    print('No route found!')
