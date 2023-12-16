057973c94c452b636984b1a7859e581b1db3edb5
sara <sarazoghi@gmail.com>
2023-12-17 02:38:15 +0330

    bfs code

--- bfs_alg.py
+++ bfs_alg.py
@@ -3,44 +3,37 @@ from testcases import *
 from succ_func import *
 import timeit
 
-def bfs(start_state: State, targets: list = target_coord.copy()):
+def bfs(start_state: State):
     visited = set()
     queue = Queue()
     queue.put(start_state)
 
     while not queue.empty():
         current_state = queue.get()
-        state_tuple = (current_state.getCurrent(), current_state.getEnergy(), tuple(current_state.getHeuVal()))
 
-        if state_tuple not in visited:
-            visited.add(state_tuple)
-                
-            if current_state.getCurrent() in targets:
-                if current_state.getEnergy() != float('-inf'):
-                    targets.remove(current_state.getCurrent())
-                    if not targets:
-                        return current_state
-                    current_state.setHeuVal(targets)
+        # Process the current state (you can add your own logic here)
+        print("Visiting:", current_state.getCurrent())
 
-            successor_states = successor_func(current_state)
-            for state in successor_states:
+        # Mark the current state as visited
+        visited.add(current_state.getCurrent())
+
+        # Generate successor states using successor_func
+        successor_states = successor_func(current_state)
+
+        for state in successor_states:
+            if state.getCurrent() not in visited:
                 queue.put(state)
 
-    return None
+    print("BFS traversal completed.")
 
+# Example usage:
 start_time = timeit.default_timer()
 
-targets = target_coord.copy()
+# Assuming you have some logic to initialize the start_state properly
+start_state = State(targets=target_coord.copy(), current=(0, 0), energy=START_ENERGY)
 
-start_state = min(successor_func(State()))
-
-final_state = bfs(start_state)
+bfs(start_state)
 
 end_time = timeit.default_timer()
-print(final_state.getInfo())
-
-if final_state.getEnergy() != float('-inf'):
-    total_time = f'{end_time - start_time} seconds'
-    print(f"Steps: {(final_state.getSteps())} \nEnergy: {final_state.getEnergy()} \nTime: {total_time}")
-else:
-    print('No route found!')
+total_time = f'{end_time - start_time} seconds'
+print(f"Time: {total_time}")
