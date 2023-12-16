eaa8d40eeb2da0f914a3d4f5f54f949596c6b85c
sara <sarazoghi@gmail.com>
2023-12-16 19:58:07 +0330

    feat : initial comiit

--- dfs_alg.py
+++ dfs_alg.py
@@ -3,55 +3,49 @@ from succ_func import State, successor_func
 import timeit
 import math
 
-def dfs_all_targets(start_state, targets):
+def dfs_best_path(start_state, targets):
     visited = set()
-    stack = [start_state]
-    max_energy_path = []
-    energy = 0;
+    best_energy_path = []
+    total_energy = 0
+    elapsed_time = 0
 
-    start_time = timeit.default_timer()
+    for target in targets:
+        stack = [start_state]
+        max_energy_path = []
 
-    while stack and set(targets) - visited:
-        current_state = stack.pop()
+        start_time = timeit.default_timer()
 
-        if current_state.getCurrent() in visited:
-            continue
+        while stack and target not in visited:
+            current_state = stack.pop()
 
-        visited.add(current_state.getCurrent())
+            if current_state.getCurrent() in visited:
+                continue
 
-        print("Visited:", current_state.getCurrent())  # Print the visited cell
+            visited.add(current_state.getCurrent())
 
-        if current_state.getCurrent() in targets:
-            targets.remove(current_state.getCurrent())  # Mark the target as visited
+            print("Visited:", current_state.getCurrent())  # Print the visited cell
 
-            # Check if all targets are visited after marking the current target
-            if not (set(targets) - visited):
+            if current_state.getCurrent() == target:
+                targets.remove(target)  # Mark the target as visited
 
                 if not max_energy_path or current_state.getEnergy() > max_energy_path[-1].getEnergy():
                     max_energy_path = current_state.getSteps() + [current_state.getCurrent()]
-                    energy = current_state.getEnergy()
 
-        successor_states = successor_func(current_state)
+        end_time = timeit.default_timer()
+        elapsed_time += end_time - start_time
+        total_energy += max_energy_path[-1].getEnergy() if max_energy_path else 0
 
-        # Sort the successor states to prioritize left-to-right, top-to-bottom order
-        successor_states.sort(key=lambda state: (state.getCurrent()[0], state.getCurrent()[1]))
+        best_energy_path += max_energy_path[:-1]  # Exclude the last cell, which is the target itself
 
-        for successor_state in successor_states:
-            next_x, next_y = successor_state.getCurrent()
+        # Set the last visited target as the new starting point
+        start_state = State(target=target)
 
-            # Check if the cell has a valid value (not -inf)
-            if not math.isinf(city_map[next_x][next_y]):
-                stack.append(successor_state)
-
-    end_time = timeit.default_timer()
-    elapsed_time = end_time - start_time
-
-    return max_energy_path, energy, elapsed_time  # Return the path with maximum energy and all targets
+    return best_energy_path, total_energy, elapsed_time
 
 
 # Example usage:
-start_state = State(target=(rows - 1, cols - 1))
-max_energy_path, total_energy, elapsed_time = dfs_all_targets(start_state, target_coord)
+start_state = State(target=(0, 0))  # Start from the first house
+max_energy_path, total_energy, elapsed_time = dfs_best_path(start_state, target_coord)
 optimal_path = convert_to_str(max_energy_path)
 if max_energy_path:
     print(total_energy, optimal_path, elapsed_time, "seconds")
