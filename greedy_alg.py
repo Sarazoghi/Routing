313f1325a7f3425af42f4e4dadba3275b38db852
mahdisoleimanii <mahdisoleimani101@gmail.com>
2023-12-17 02:30:39 +0330

    greedy algorithm fixed based on new successor function

--- greedy_alg.py
+++ greedy_alg.py
@@ -4,40 +4,39 @@ import timeit
 import math
 
 def bestHeuVal(first: State, second: State):
-    if first.getHeuVal() <= second.getHeuVal():
+    if sum(first.getHeuVal()) <= sum(second.getHeuVal()):
         return first
     else:
         return second
     
-def greedy(start_state: State):
-    if start_state.getCurrent() == start_state.getTarget():
-        return start_state
+def greedy(start_state: State, targets: list = target_coord.copy()):
+    if start_state.getEnergy() < most_cost:
+        return None
     else:
-        successor_states = successor_func(start_state)
-        best_next_state = successor_states[0]
-        for state in successor_states[1:]:
-            best_next_state = bestHeuVal(best_next_state, state)
-        if best_next_state.getHeuVal() > start_state.getHeuVal():
-            return None
-        return greedy(best_next_state)
+        if start_state.getCurrent() in targets:
+            targets.remove(start_state.getCurrent())
+            if not targets:
+                return start_state
+            start_state.setHeuVal(targets)
+            return greedy(start_state, targets)
+        else:
+            successor_states = successor_func(start_state)
+            best_next_state = successor_states[0]
+            for state in successor_states[1:]:
+                best_next_state = bestHeuVal(best_next_state, state)
+            return greedy(best_next_state)
 
 start_time = timeit.default_timer()
 
 targets = target_coord.copy()
-start_states = []
-for target in target_coord:
-    start_states.extend(successor_func(State(target)))
 
-final_state = min(start_states)
-while targets and final_state:
-    best_next_target = min(targets, key=lambda target: manhat_heu(final_state.getCurrent(), target))
-    final_state.setTarget(best_next_target)
-    final_state = greedy(final_state)
-    targets.remove(best_next_target)
+start_state = min(successor_func(State()))
+
+final_state = greedy(start_state)
 
 end_time = timeit.default_timer()
 
-if final_state.getEnergy() != float('-inf'):
+if final_state and final_state.getEnergy() != float('-inf'):
     total_time = f'{end_time - start_time} seconds'
     print(f"Steps: {convert_to_str(final_state.getSteps())} \nEnergy: {final_state.getEnergy()} \nTime: {total_time}")
 else:
